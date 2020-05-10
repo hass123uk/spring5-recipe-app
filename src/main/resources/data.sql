254e1b7d7174089201988eec848013c83773734a
Hassan Mahmud <hassanmahmud93@outlook.com>
2020-05-10 19:16:20 +0200

    Add UOM & Categories.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 INSERT INTO category (description) VALUES ('Libyan');
 INSERT INTO category (description) VALUES ('BBQ');
-INSERT INTO category (description) VALUES ('Greek');
+INSERT INTO category (description) VALUES ('American');
 INSERT INTO category (description) VALUES ('Fast food');
 INSERT INTO category (description) VALUES ('Mexican');
 
@@ -9,6 +9,6 @@ INSERT INTO unit_of_measure (description) VALUES ('Tablespoon');
 INSERT INTO unit_of_measure (description) VALUES ('Cup');
 INSERT INTO unit_of_measure (description) VALUES ('Pinch');
 INSERT INTO unit_of_measure (description) VALUES ('Ounce');
-INSERT INTO unit_of_measure (description) VALUES ('Pieces');
+INSERT INTO unit_of_measure (description) VALUES ('Each');
 INSERT INTO unit_of_measure (description) VALUES ('Dash');
 INSERT INTO unit_of_measure (description) VALUES ('Pint');
