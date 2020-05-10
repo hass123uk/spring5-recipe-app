00b098922dc31a95323ac929c6f2a09f54df144d
Hassan Mahmud <hass123uk@hotmail.com>
2020-05-10 13:39:29 +0200

    Init Spring boot web app, add index page, start on entity model.

---
+++ src/main/java/com/hassanmahmud/hmrecipeproject/HmRecipeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.hassanmahmud.hmrecipeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmRecipeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HmRecipeProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hassanmahmud/hmrecipeproject/HmRecipeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hassanmahmud.hmrecipeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmRecipeProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
