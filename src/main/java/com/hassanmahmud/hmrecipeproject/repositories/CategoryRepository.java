cd4ea6454191a04e2570f3e202fd2f94a486f71d
Hassan Mahmud <hass123uk@hotmail.com>
2020-05-10 18:01:05 +0200

    Add findByDescription to Category and UnitOfMeasure repositories.

--- src/main/java/com/hassanmahmud/hmrecipeproject/repositories/CategoryRepository.java
+++ src/main/java/com/hassanmahmud/hmrecipeproject/repositories/CategoryRepository.java
@@ -3,5 +3,9 @@ package com.hassanmahmud.hmrecipeproject.repositories;
 import com.hassanmahmud.hmrecipeproject.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Optional<Category> findByDescription(String description);
 }
--- src/main/java/com/hassanmahmud/hmrecipeproject/repositories/UnitOfMeasureRepository.java
+++ src/main/java/com/hassanmahmud/hmrecipeproject/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,9 @@ package com.hassanmahmud.hmrecipeproject.repositories;
 import com.hassanmahmud.hmrecipeproject.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
