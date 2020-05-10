ae1234af4877754923a18366a6257531450172a9
Hassan Mahmud <hass123uk@hotmail.com>
2020-05-10 16:09:22 +0200

    Add data.sql to Seed initial project data.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO category (description) VALUES ('Libyan');
+INSERT INTO category (description) VALUES ('BBQ');
+INSERT INTO category (description) VALUES ('Greek');
+INSERT INTO category (description) VALUES ('Fast food');
+INSERT INTO unit_of_measure (description) VALUES ('Teaspoon');
+INSERT INTO unit_of_measure (description) VALUES ('Tablespoon');
+INSERT INTO unit_of_measure (description) VALUES ('Cup');
+INSERT INTO unit_of_measure (description) VALUES ('Pinch');
+INSERT INTO unit_of_measure (description) VALUES ('Ounce');
