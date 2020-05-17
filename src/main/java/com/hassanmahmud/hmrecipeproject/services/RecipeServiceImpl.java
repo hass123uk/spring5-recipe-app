49ac380cc6ab2b8fdd49630b45d0c53dea8df05a
Hassan Mahmud <hass123uk@hotmail.com>
2020-05-17 16:45:26 +0200

    Refactor project to use Lombok - Recipe uses builder.

--- src/main/java/com/hassanmahmud/hmrecipeproject/services/RecipeServiceImpl.java
+++ src/main/java/com/hassanmahmud/hmrecipeproject/services/RecipeServiceImpl.java
@@ -2,22 +2,23 @@ package com.hassanmahmud.hmrecipeproject.services;
 
 import com.hassanmahmud.hmrecipeproject.domain.Recipe;
 import com.hassanmahmud.hmrecipeproject.repositories.RecipeRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class RecipeServiceImpl implements RecipeService {
 
     private final RecipeRepository recipeRepository;
 
-    public RecipeServiceImpl(RecipeRepository recipeRepository) {
-        this.recipeRepository = recipeRepository;
-    }
-
     @Override
     public Set<Recipe> getAll() {
+        log.debug("I'm in the service");
         var recipes = new HashSet<Recipe>();
         recipeRepository.findAll().forEach(recipes::add);
         return recipes;
